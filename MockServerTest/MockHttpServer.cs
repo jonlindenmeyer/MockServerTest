82839bdd7150ffc8de8ed5994f062ac9a345f3dd
Jonas Lindenmeyer <ext.jonas.lindenmeyer@farfetch.com>
2020-04-28 21:47:24 +0100

    mockserver test

---
+++ MockServerTest/MockHttpServer.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace MockServerTest
+{
+    public static class MockHttpServer
+    {
+        private static readonly JsonSerializerSettings JsonSerializerSettings = new JsonSerializerSettings
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            NullValueHandling = NullValueHandling.Ignore,
+            Formatting = Formatting.Indented
+        };
+
+        private static readonly HttpClient HttpClient = new HttpClient();
+
+        public static void Reset(params string[] urls)
+        {
+            urls.ToList()
+                .ForEach(url => HttpClient.PutAsync(new Uri($"{url}/reset"), null));
+        }
+
+        public static async Task<object> CreateExpectationAsync(
+            string endpoint,
+            string method,
+            string path,
+            IEnumerable<(string, string)> headers = null,
+            IEnumerable<(string, string)> query = null,
+            object requestBody = null,
+            object responseBody = null,
+            int remainingTimes = 0,
+            int statusCode = 200)
+        {
+            var expectation = new
+            {
+                HttpRequest = CreateHttpRequest(
+                    method,
+                    path,
+                    headers ?? Enumerable.Empty<(string, string)>(),
+                    query ?? Enumerable.Empty<(string, string)>(),
+                    requestBody),
+                HttpResponse = CreateHttpResponse(responseBody, statusCode),
+                Times = new
+                {
+                    RemainingTimes = remainingTimes,
+                    Unlimited = remainingTimes == 0
+                }
+            };
+
+            using (var content = new StringContent(JsonConvert.SerializeObject(expectation, JsonSerializerSettings)))
+            {
+                var response = await HttpClient.PutAsync(new Uri(new Uri(endpoint), "/expectation"), content);
+
+                if (response.StatusCode != HttpStatusCode.Created)
+                {
+                    var builder = new StringBuilder();
+
+                    builder.AppendLine("Failed to setup expectation:");
+                    builder.Append("expectation: ");
+                    builder.AppendLine(JsonConvert.SerializeObject(expectation, JsonSerializerSettings));
+                    builder.Append("response: ");
+                    builder.AppendLine(JsonConvert.SerializeObject(response, JsonSerializerSettings));
+
+                    throw new InvalidOperationException(builder.ToString());
+                }
+
+                return expectation;
+            }
+        }
+
+        private static object CreateHttpResponse(object body, int statusCode) => new
+        {
+            StatusCode = statusCode,
+            Headers = new[]
+                {
+                    new { Name = "content-type", Values = new[] { "application/json" } }
+                },
+            Body = body != null ? new { Type = "JSON", Json = JsonConvert.SerializeObject(body) } : null
+        };
+
+        private static object CreateHttpRequest(
+            string method,
+            string path,
+            IEnumerable<(string, string)> headers,
+            IEnumerable<(string, string)> query,
+            object body) => new
+            {
+                Method = method,
+                Path = path,
+                Headers = headers.Select(t => new { Name = t.Item1, Values = new[] { t.Item2 } }),
+                QueryStringParameters = query.Select(t => new { Name = t.Item1, Values = new[] { t.Item2 } }),
+                KeepAlive = true,
+                Secure = false,
+                Body = body != null ? new { Type = "JSON", Json = JsonConvert.SerializeObject(body) } : null
+            };
+
+        public static async Task<bool> Verify(string endpoint, string path, string method)
+        {
+            var verify = new
+            {
+                HttpRequest = new { path, method },
+                Times = new { atLeast = 1 }
+            };
+
+            using (var content = new StringContent(JsonConvert.SerializeObject(verify, JsonSerializerSettings), Encoding.UTF8))
+            {
+                var response = await HttpClient.PutAsync(new Uri(new Uri(endpoint), "/mockserver/verify"), content);
+                return response.IsSuccessStatusCode;
+            }
+        }
+    }
+}
---
+++ MockServerTest/MockTest.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MockServerTest
+{
+    [TestClass]
+    public class MockTest
+    {
+        private const string MOCKURL = "http://localhost:7777/";
+        private HttpClient httpClient;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            MockHttpServer.Reset(MOCKURL);
+            httpClient = new HttpClient();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            MockHttpServer.Reset(MOCKURL);
+        }
+
+        [TestMethod]
+        public async Task SendExpectationAndVerify_Success()
+        {
+            // Arrange
+            var dummy = new { Id = Guid.NewGuid(), Name = "Dummy Mock Test" };
+
+            var path = "/mocktest";
+
+            var method = "GET";
+
+            var expectation = await MockHttpServer.CreateExpectationAsync(
+                MOCKURL,
+                method,
+                path,
+                responseBody: dummy,
+                statusCode: 200);
+
+            // Act
+            // aqui deveria ter a chamada do client da sua aplicacao, ou envio de evento, ou algo que dispare o uso do mockserver
+            // vamos simular com um simples get
+            await httpClient.GetAsync($"{MOCKURL}{path}");
+
+            // Assert
+            Assert.IsNotNull(expectation, "Expectation null");
+
+            var verify = await MockHttpServer.Verify(MOCKURL, path, method);
+
+            Assert.IsTrue(verify, "Mock verify failure");
+        }
+    }
+}
